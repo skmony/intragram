f7db8be458fee5877ff41cf4f9d90e0893b05a90
Shashikant Mony <shashikantmony46@gmail.com>
2019-11-05 00:09:45 +0530

    Initial commit

---
+++ src/app/auth/loginp/loginp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginp',
+  templateUrl: './loginp.component.html',
+  styleUrls: ['./loginp.component.css']
+})
+export class LoginpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
